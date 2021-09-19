c5ae6b20b4f3e4ce33ea760862f16924ebad4ae2
SilanSurfer <szwarc.adam@gmail.com>
2021-09-20 00:22:04 +0200

    feat: Created InputRecord and OutputRecord structures for input and output records

---
+++ src/data_records.rs
@@ -0,0 +1,19 @@
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct InputRecord {
+    // TODO: It should be serialized from field named "type"
+    oper_type: String,
+    client: u16,
+    tx: u32,
+    amount: f32,
+}
+
+#[derive(Debug, Serialize)]
+pub struct OutputRecord {
+    client: u16,
+    available: f32,
+    held: f32,
+    total: f32,
+    locked: bool,
+}
