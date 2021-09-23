9f25a3452a26373c347c224bc6ffd3a061eea296
SilanSurfer <szwarc.adam@gmail.com>
2021-09-23 14:17:52 +0200

    feat: Integrated parsing operations in main function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,30 @@
 mod cli;
 mod data_records;
+mod engine;
+mod errors;
+mod operations;
 
-fn main() {
-    let opt = cli::Opt::get_input_file();
-    println!("{:?}", opt);
+use engine::Engine;
+
+use std::fs::File;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let input_file_path = cli::Opt::get_input_file();
+    let mut engine: Engine = Engine::new();
+    let file = File::open(input_file_path)?;
+    let mut reader = csv::Reader::from_reader(file);
+    for result in reader.deserialize() {
+        match result {
+            Ok(record) => {
+                println!("{:?}", record);
+                if let Err(e) = engine.process_record(record) {
+                    eprintln!("Error: {:?}", e);
+                }
+            }
+            Err(e) => {
+                eprintln!("Error while deserializing record! {}", e);
+            }
+        }
+    }
+    Ok(())
 }
